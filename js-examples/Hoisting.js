3b0da39742c0ab67d8ce63471a9b0eeebac8a0f8
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-11 19:55:17 +0530

    hoisting

---
+++ js-examples/Hoisting.js
@@ -0,0 +1,15 @@
+ console.log(a)//ReferenceError: a is not defined
+ console.log(b)//ReferenceError: b is not defined
+console.log(c)//undefined
+
+
+{
+    let a=10;
+    const b=20;
+    var c =30;
+
+    
+     console.log(a,b,c);
+
+}
+
