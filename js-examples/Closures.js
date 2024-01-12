a2cc2f29f0a278b1a7488fd6228de56ec783e808
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-12 21:39:09 +0530

    commented out

--- js-examples/Closures.js
+++ js-examples/Closures.js
@@ -1,28 +1,29 @@
 
  
-    function X(){
+//     function X(){
         
-            for(var i=0;i<=5;i++){
-                function close(i){
-                setTimeout(()=>{
-                    console.log(i)
+//             for(var i=0;i<=5;i++){
+//                 function close(i){
+//                 setTimeout(()=>{
+//                     console.log(i)
             
-                },i*2000)
-        }
-        close(i);
- }
+//                 },i*2000)
+//         }
+//         close(i);
+//  }
  
-}
-X();
+// }
+// X();
 //if use let instead of var in forloop, there is no need of inner function close
 
 //eg:2
 function outer(){
-    var a=10
+    
     function inner(){
         console.log(a)
 
     }
+    let a=10
     return inner;//return is 
 }
 outer()();
