0b7115d76b9180564aa4525e88f09698dba0fc93
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-12 18:43:47 +0530

    scope chain

--- js-examples/ScopeChain.js
+++ js-examples/ScopeChain.js
@@ -5,6 +5,7 @@ function x(){
     function Y(){
         console.log(a)  
   //js engine first try to find a in the local environment of Y. then check its lexical envmnt.
+  //this process of finding a variable in each level is called scope chain 
  
      }
     // return Y;
