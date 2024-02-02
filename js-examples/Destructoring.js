7e2d9ba5c1b97b3eadfc1dc22835c625f4ef9891
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-02-02 22:43:16 +0530

    destructoring

---
+++ js-examples/Destructoring.js
@@ -0,0 +1,10 @@
+//array destructoring
+const[firstName,lastName]=[ "niv",'huf'];
+console.log(firstName);
+
+//object destructoring
+const {name,age} = {
+    name:"niv",age:"1"
+}
+console.log(name);
+
